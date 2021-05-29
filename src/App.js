990e09e431813ebfaca23dbbdd754160e53e07fc
Jacob <azzizi.m.yakoub@gmail.com>
2021-05-30 00:48:24 +0100

    update our menu items to mavigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Homepage from "./pages/homepage/homepage.component";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Homepage />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
